7f4917ca0d363050c8d63cd85289ad935c8c1556
Phyllis111-cxy <xc3342@nyu.edu>
2026-08-14 15:13:54 +0800

    Prepare GitHub Pages (src/App.tsx)

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,11 @@ import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import { LangdonPage } from '@/pages/LangdonPage'
 import { SophiePage } from '@/pages/SophiePage'
 
+const basename = import.meta.env.BASE_URL.replace(/\/$/, '') || undefined
+
 export default function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={basename}>
       <Routes>
         <Route path="/" element={<LangdonPage />} />
         <Route path="/langdon" element={<Navigate to="/" replace />} />
